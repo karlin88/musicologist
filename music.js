9d7ab7117ff14a435b3f59df05807a91881ab78d
Linda Pei <linda.pei@gmail.com>
2017-09-11 13:43:09 -0700

    completed

--- music.js
+++ music.js
@@ -32,5 +32,31 @@ function renderList() {
 $('#getPlaylistBtn').click(function (event) {
   // TODO: Display a list of music.
   // You may use anything from musicInfo.
+  var searchterm = ""
+ // console.log($('.info').children().text())
+  for( term of $('.info').children() ) {
+    //console.log(term.innerHTML );
+    if (searchterm =="") {
+      searchterm = term.innerText;
+    } else {
+      searchterm = searchterm + "+" + term.innerText;
+    }
+  }
+  console.log(searchterm)
+
+  const $recomm = $('.info').eq(0);
+  $recomm.empty();
+
+  $.get( "https://itunes.apple.com/search?term="+searchterm+"&media=music", function ( data ) {
+      var music = JSON.parse(data);
+      var trackName = music.results.map( function (obj) { return (obj.trackName)});
+      for (const track of trackName) {
+        const $item = $('<li class="recommend-item">').text(track);
+        $recomm.append($item)
+      }
+    });
+
+  //console.log($playlist);
+
   console.log('Testing Music Call');
 });
